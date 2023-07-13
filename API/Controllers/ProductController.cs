b5b789dbfd00fa57152b2f9d7c2fca6ee78fc7e9
404Exception <urfiusmani007@gmail.com>
2023-07-13 20:58:18 +0530

    Section 2

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public ProductsController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _dataContext.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product =  await _dataContext.Products.FindAsync(id);
+            
+            return product;
+        }
+    }
+}
