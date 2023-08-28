a6d6ccc88e7d7f667b7001abbe555035d2994aa3
404Exception <urfiusmani007@gmail.com>
2023-08-29 02:27:24 +0530

    Binding the UI of shop

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './Model/product';
-import { Pagination } from './Model/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,20 +7,9 @@ import { Pagination } from './Model/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {
+  constructor() {}
 
-  }
-
-  ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pagesize=50').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.error(error),
-      complete: () => {
-        console.log('Request completed');
-        console.log('Extra statment');
-      }
-    })
+  ngOnInit(): void {    
   }
 }
